6e53ff4ab482738cf0a80c80067706abdf078a20
Felix Geller <fgeller@gmail.com>
2016-09-18 08:48:08 +1200

    emacs: adds helper for ag with thing at point.

--- .emacs.d/search.el
+++ .emacs.d/search.el
@@ -6,3 +6,8 @@
   (interactive)
   (let ((thing (thing-at-point 'symbol)))
     (ag-project thing)))
+
+(defun ag-with-thing-at-point ()
+  (interactive)
+  (let ((thing (thing-at-point 'symbol)))
+    (ag thing default-directory)))
