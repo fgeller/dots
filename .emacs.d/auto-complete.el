7978b5e339a7e8911afeaf534d1d864674aecd91
Felix Geller <fgeller@gmail.com>
2016-09-18 08:43:04 +1200

    emacs: tweaks company mode config.

--- .emacs.d/auto-complete.el
+++ .emacs.d/auto-complete.el
@@ -2,6 +2,10 @@
   :defer 5
   :config
   (global-company-mode 1)
+  (setq company-backends '(company-bbdb company-capf company-files company-elisp company-dabbrev))
+  (setq company-idle-delay .05)
+  (setq company-tooltip-idle-delay .05)
+  (setq company-tooltip-align-annotations t)
   (define-key company-mode-map (kbd "C-n") 'company-select-next)
   (define-key company-mode-map (kbd "C-p") 'company-select-previous))
 
