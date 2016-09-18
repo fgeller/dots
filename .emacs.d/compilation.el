86c16e4079f47e0ce7099a421ac66a2a7289319f
Felix Geller <fgeller@gmail.com>
2016-09-18 16:27:37 +1200

    emacs: interpret escape sequences in compilation buffer.

--- .emacs.d/compilation.el
+++ .emacs.d/compilation.el
@@ -1,3 +1,12 @@
 
-(defun enable-font-lock () (font-lock-mode 1))
-(add-hook 'compilation-mode-hook 'enable-font-lock)
+(require 'ansi-color)
+(defun ansi-colorize-buffer ()
+  (let ((buffer-read-only nil))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+(add-hook 'compilation-filter-hook 'ansi-colorize-buffer)
+
+(defun compilation-customizations ()
+  (font-lock-mode 1)
+  (setq show-trailing-whitespace nil))
+
+(add-hook 'compilation-mode-hook 'compilation-customizations)
