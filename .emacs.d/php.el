cdcedb6a7574d1a45854943568eb9ca719bcf823
Felix Geller <fgeller@gmail.com>
2016-10-29 14:55:21 +1300

    emacs: drops superfluous php customizations.

--- .emacs.d/php.el
+++ .emacs.d/php.el
@@ -1,9 +1,3 @@
 (use-package php-mode
   :ensure php-mode
   :commands php-mode)
-
-(defun php-customizations ()
-  (setq tab-width 4)
-  (add-hook 'before-save-hook 'maybe-cleanup-whitespace nil 'local))
-
-(add-hook 'php-mode-hook 'php-customizations)
