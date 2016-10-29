45bf683c20648dac1b9d982f520677a13982ae3c
Felix Geller <fgeller@gmail.com>
2016-10-29 15:07:18 +1300

    emacs: cut down scratch config.

--- .emacs.d/scratch.el
+++ .emacs.d/scratch.el
@@ -1,21 +1,6 @@
-;; auto-save and auto-load scratch buffer
-;; http://dorophone.blogspot.co.nz/2011/11/how-to-make-emacs-scratch-buffer.html
-
-(defvar persistent-scratch-filename "~/.emacs.d/scratch" "Location of *scratch* file contents.")
-(defvar persistent-scratch-backup-directory "~/.emacs.d/scratch-backups/" "Location of backups of the *scratch* buffer contents.")
-
-(defun make-persistent-scratch-backup-name ()
-  "Create a filename to backup the current scratch file by
-  concatenating PERSISTENT-SCRATCH-BACKUP-DIRECTORY with the
-  current date and time."
-  (concat
-   persistent-scratch-backup-directory
-   (format-time-string "%Y-%m-%dT%H:%M:%S" (current-time))))
+(defvar persistent-scratch-filename "~/.emacs.d/scratch")
 
 (defun save-persistent-scratch ()
-  "Write the contents of *scratch* to the file name
-  PERSISTENT-SCRATCH-FILENAME, making a backup copy in
-  PERSISTENT-SCRATCH-BACKUP-DIRECTORY."
   (with-current-buffer (get-buffer "scratch")
     (basic-save-buffer)))
 
