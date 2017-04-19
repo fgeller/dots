b8fdbc5afd4ce3bd4cc72f7cadb7c1367057bd0d
Felix Geller <fgeller@gmail.com>
2017-04-19 10:08:39 +0200

    emacs: just follow symlinks under vc without asking

--- .emacs.d/vc.el
+++ .emacs.d/vc.el
@@ -1,6 +1,8 @@
 (add-hook 'vc-annotate-mode-hook
 	  (lambda () (setq show-trailing-whitespace nil)))
 
+(setq vc-follow-symlinks t)
+
 (defun vc-git-annotate-command (file buf &optional rev)
   (let ((name (file-relative-name file)))
     (vc-git-command buf 'async nil "blame" "--date=short" "-C" "-C" rev "--" name)))
