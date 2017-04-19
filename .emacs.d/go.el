64e26022419daddf3864893decdb464b479de72a
Felix Geller <fgeller@gmail.com>
2017-04-19 10:12:34 +0200

    emacs: naively adds gosimple and unused flycheck checkers.

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -29,6 +29,28 @@
   (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'after-save-hook #'go-after-save-run-tests))
 
+(require-package 'flycheck)
+(require 'flycheck)
+(flycheck-define-checker go-unused
+  ""
+  :command ("unused" ".")
+  :error-patterns
+  ((warning line-start (file-name) ":" line ":" column ": " (message) line-end))
+  :modes go-mode
+  :predicate flycheck-buffer-saved-p)
+(flycheck-define-checker go-gosimple
+  ""
+  :command ("gosimple" ".")
+  :error-patterns
+  ((warning line-start (file-name) ":" line ":" column ": " (message) line-end))
+  :modes go-mode
+  :predicate flycheck-buffer-saved-p)
+
+(add-to-list 'flycheck-checkers 'go-gosimple)
+(add-to-list 'flycheck-checkers 'go-unused)
+(flycheck-add-next-checker 'go-test 'go-gosimple 'append)
+(flycheck-add-next-checker 'go-test 'go-unused 'append)
+
 (defun go-tests-buffer-name ()
   (format "*go-test[%s]*"
 	  (file-name-nondirectory (directory-file-name default-directory))))
