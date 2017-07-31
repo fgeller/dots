ca556be1c5d0cdeba4050006260c467998d9c9be
Felix Geller <fgeller@gmail.com>
2017-07-31 10:03:15 +0200

    emacs: adds rjsx-mode.

--- .emacs.d/javascript.el
+++ .emacs.d/javascript.el
@@ -1,4 +1,6 @@
-(install 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(install 'rjsx-mode)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
 (add-hook 'js2-mode-hook (lambda () (font-lock-mode 1)))
+(add-hook 'rjsx-mode-hook (lambda () (font-lock-mode 1)))
 (setq js2-basic-offset 2)
