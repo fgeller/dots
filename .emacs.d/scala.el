16bd582105fb1cdc819506136c448dae6d624165
Felix Geller <fgeller@gmail.com>
2017-09-04 12:29:41 +0200

    emacs: resurrects ensime.

--- .emacs.d/scala.el
+++ .emacs.d/scala.el
@@ -1,9 +1,17 @@
+(install 'ensime)
+(install 'sbt-mode)
 (install 'scala-mode)
 (add-hook 'scala-mode-hook 'scala-customizations)
+
+(setq ensime-startup-notification nil
+      ensime-startup-snapshot-notification nil)
 
 (defun scala-customizations ()
   (setq tab-width 2)
   (font-lock-mode -1)
+  (setq ensime-sem-high-enabled-p t)
+  (setq ensime-eldoc-hints 'all)
+  (setq ensime-typecheck-when-idle nil)
   (subword-mode 1)
   (yas-minor-mode 1))
 
