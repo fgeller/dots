93b2a552d94fc7b048f45da19aa482d2fc6ab71f
Felix Geller <fgeller@gmail.com>
2017-09-06 12:02:49 +0200

    emacs: drops unused config.

--- .emacs.d/web-mode.el
+++
@@ -1,9 +0,0 @@
-(install 'web-mode)
-(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
-(setq web-mode-enable-auto-closing nil)
-(add-hook 'web-mode-hook 'web-mode-customizations)
-
-(defun web-mode-customizations ()
-  (yas-minor-mode 1))
-
-
