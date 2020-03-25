2b39214caa3d526c6898575d0b28b1674553615f
Felix Geller <fgeller@gmail.com>
2020-03-25 21:31:14 +0100

    emacs: render all lsp info in minibuffer

--- .emacs.d/lsp.el
+++ .emacs.d/lsp.el
@@ -3,5 +3,9 @@
 (install 'company-lsp)
 
 (setq
+ lsp-session-file "~/.emacs.d/transient/lsp-session-v1"
+ lsp-eldoc-render-all t
+ lsp-ui-doc-enable nil
+ lsp-ui-sideline-enable nil
  lsp-prefer-capf t
  lsp-idle-delay 0.2)
