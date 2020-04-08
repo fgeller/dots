52f1cfd33e0799a2ab55a61c34bcfb6b714cfbd2
Felix Geller <fgeller@gmail.com>
2020-04-08 20:53:00 +0200

    emacs: increase auto-complete delay

--- .emacs.d/auto-complete.el
+++ .emacs.d/auto-complete.el
@@ -2,8 +2,8 @@
 
 (setq
  company-backends '(company-capf company-files company-elisp company-dabbrev)
- company-idle-delay .12
- company-tooltip-idle-delay .12
+ company-idle-delay .75
+ company-tooltip-idle-delay .75
  company-tooltip-align-annotations t)
 
 (after 'company
