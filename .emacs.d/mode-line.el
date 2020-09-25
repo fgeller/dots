585f3f65bdf360cd4ea19745b622d960647f4464
Felix Geller <fgeller@gmail.com>
2020-09-25 11:20:47 +0200

    appearance tweaks

--- .emacs.d/mode-line.el
+++ .emacs.d/mode-line.el
@@ -1,10 +1,10 @@
 (defconst mode-line-wanted-minor-modes '(lsp-mode flycheck-mode))
 
 (setq-default header-line-format
-	      `(
+	      `(		
 		" " (:propertize "%b" face mode-line-buffer-id)
-		" " "%1*"
-		" " "%l" "/" "%c"
+		(:propertize "%1*" face mode-line)
+		(:propertize " %l:%c" face mode-line)
 		" " ,(cl-remove-if-not
 		      (lambda (p) (member (car p) mode-line-wanted-minor-modes))
 		      minor-mode-alist)
@@ -12,15 +12,17 @@
 		" "
 		))
 
-(setq-default mode-line-format nil)
+(setq-default mode-line-format "")
 
 (defun mode-line-bell ()
   (let ((orig-active (face-attribute 'mode-line :background))
 	(orig-inactive (face-attribute 'mode-line-inactive :background)))
-    (set-face-attribute 'mode-line nil :background "#D84315")
-    (set-face-attribute 'mode-line-inactive nil :background "#D84315")
+    (set-face-attribute 'mode-line nil :background "#ff0000")
+    (set-face-attribute 'mode-line-highlight nil :background "#ff0000")
+    (set-face-attribute 'mode-line-inactive nil :background "#ff0000")
     (sit-for 0.1 t)
     (set-face-attribute 'mode-line nil :background orig-active)
+    (set-face-attribute 'mode-line-highlight nil :background orig-active)
     (set-face-attribute 'mode-line-inactive nil :background orig-inactive)))
 
 (setq
