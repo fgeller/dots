a31f31a5226ca329f61a7daa976f9c66bcfc14b4
Felix Geller <fgeller@gmail.com>
2020-10-08 21:13:51 +0200

    emacs: same mode/header-line tweaks triggered by @rougier's nano.el

--- .emacs.d/mode-line.el
+++ .emacs.d/mode-line.el
@@ -1,29 +1,37 @@
 (defconst mode-line-wanted-minor-modes '(lsp-mode flycheck-mode))
 
+
+;; via https://gist.github.com/rougier/4d06d892ded73b4dc6b364a8e0fbcad5
+(defun header-line-render (left right)
+   (let* ((available-width (- (window-total-width) (length left) )))
+     (format (format "%%s%%%ds" available-width) left right)))
+
 (setq-default header-line-format
-	      `(		
-		" " (:propertize "%b" face mode-line-buffer-id)
-		(:propertize "%1*" face mode-line)
-		(:propertize " %l:%c" face mode-line)
-		" " ,(cl-remove-if-not
-		      (lambda (p) (member (car p) mode-line-wanted-minor-modes))
-		      minor-mode-alist)
-		" " mode-line-process
-		" "
-		))
+	      `((:eval
+		 (header-line-render
+		  (format-mode-line
+		   (list
+		    (propertize " %b" 'face '(:weight bold))
+		    "%1* "
+		    (propertize " " 'display '(raise +0.25))
+		    (propertize " " 'display '(raise -0.30))
+		    ))
+		  (format-mode-line
+		   (list
+		    " " (cl-remove-if-not
+			  (lambda (p) (member (car p) mode-line-wanted-minor-modes))
+			  minor-mode-alist)
+		    " " mode-line-process
+		    " %l:%c "))))))
 
 (setq-default mode-line-format "")
 
 (defun mode-line-bell ()
   (let ((orig-active (face-attribute 'mode-line :background))
 	(orig-inactive (face-attribute 'mode-line-inactive :background)))
-    (set-face-attribute 'mode-line nil :background "#ff0000")
-    (set-face-attribute 'mode-line-highlight nil :background "#ff0000")
-    (set-face-attribute 'mode-line-inactive nil :background "#ff0000")
+    (set-face-attribute 'mode-line nil :background "#F44336")
     (sit-for 0.1 t)
-    (set-face-attribute 'mode-line nil :background orig-active)
-    (set-face-attribute 'mode-line-highlight nil :background orig-active)
-    (set-face-attribute 'mode-line-inactive nil :background orig-inactive)))
+    (set-face-attribute 'mode-line nil :background orig-active)))
 
 (setq
  visible-bell nil
@@ -42,5 +50,3 @@
                    faces-to-toggle)))))
 
 (add-hook 'modal-mode-hook 'modal-mode-visual-toggle)
-
-
