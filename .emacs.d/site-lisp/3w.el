24821de32c8f1764ce7d558490d73dacd0a2dfdb
Felix Geller <fgeller@gmail.com>
2020-12-09 10:53:00 +0100

    emacs: more work on 3w

--- .emacs.d/site-lisp/3w.el
+++ .emacs.d/site-lisp/3w.el
@@ -12,69 +12,115 @@
 (defconst 3w-side-window-rx (rx (or "*Backtrace*" "*Compilation*" "*Faces*" "*Help*" "*Messages*" "*Occur*" "*Warnings*" "*ag search"))
   "Regex that matches buffer names that should be displayed in a side window")
 
+(defun 3w-get-side-window-buffer ()
+  "Returns buffer displayed in side window if it matches `3w-side-window-rx'"
+  (let* ((is-cs (3w-is-column-split-p))
+	 (sw (car (window-at-side-list nil (if is-cs 'right 'bottom)))))
+    (when (3w-side-window-name-p sw) (window-buffer sw))))
+
+(defun 3w-store-side-buffer ()
+  (let* ((sb (3w-get-side-window-buffer)))
+    (when sb
+      (setq 3w-side-buffer-store sb)
+      (message "3w: stored side window buffer %s" sb))))
+
+(defun 3w-side-window-name-p (w)
+  (3w-side-buffer-name-p (window-buffer w)))
+
+(defun 3w-side-buffer-name-p (b)
+  (string-match-p 3w-side-window-rx (buffer-name b)))
+
+(defun 3w-is-column-split-p ()
+  (= (window-size) (1- (frame-total-lines))))
+
 (defun 3w-should-split-into-columns-p (count)
   "Indicates whether there is enough space for 3w to split windows into columns"
   (> (frame-total-cols)
      (* count 3w-minimal-window-width)))
 
-(defun 3w-get-side-window-buffer ()
-  "Returns buffer displayed in side window if it matches `3w-side-window-rx'"
-  (let* ((is-cs (3w-is-column-split-p))
-	 (rw (car (window-at-side-list nil 'right)))
-	 (bw (car (window-at-side-list nil 'bottom)))
-	 (bf (if is-cs (window-buffer rw) (window-buffer bw)))
-	 (bn (buffer-name bf)))
-    (when (string-match-p 3w-side-window-rx bn)
-      bf)))
+(defun 3w-split-window (count &optional size)
+  (if (3w-should-split-into-columns-p count)
+      (split-window-right size)
+    (split-window-below size)))
 
 (defun 3w-split-1 ()
   "Maximizes current window, optionally storing a side window's buffer if present"
   (interactive)
-  (let ((buf (3w-get-side-window-buffer)))
-    (when buf
-      (setq 3w-side-buffer-store buf)
-      (message "3w: stored side window buffer %s" buf)))
+  (3w-store-side-buffer)
   (delete-other-windows))
 
-(defun 3w-is-column-split-p ()
-  (= (window-size) (1- (frame-total-lines))))
-
-(defun 3w-split-1-1 ()
+(defun 3w-split-2 ()
   "Produces two evenly split windows"
   (interactive)
   (let* ((should-cs (3w-should-split-into-columns-p 2))
-	 (is-cs (3w-is-column-split-p))
-	 (wc (count-windows)))
-    (cond ((= 1 wc)
-	   (let* ((nw (if should-cs (split-window-right) (split-window-below)))
-		  (ob (or 3w-side-buffer-store (other-buffer))))
-	     (when ob (set-window-buffer nw ob))
-	     (balance-windows)))
+	 (size (round (/ (if should-cs (frame-total-cols) (frame-total-lines)) 2))))
+    (3w-split-2-with-size size)
+    (balance-windows)))
 
+(defun 3w-split-2-1 ()
+  "Produces two split windows where the left/top window is 2/3 of the frame size"
+  (interactive)
+  (let* ((should-cs (3w-should-split-into-columns-p 2))
+	 (size (round (* 0.667 (if should-cs (frame-total-cols) (frame-total-lines)) ))))
+    (3w-split-2-with-size size)))
+
+(defun 3w-split-2-with-size-from-1 (size &optional next-buffer)
+  (delete-other-windows)
+  (let* ((nb (or next-buffer 3w-side-buffer-store (other-buffer)))
+	 (nw (3w-split-window 2 size)))
+    (set-window-buffer nw nb)))
+   
+(defun 3w-split-2-with-size (size)
+  "Produces two split windows with the given size for the right/bottom window"
+  (3w-store-side-buffer)
+  (let* ((wc (count-windows)))
+    (cond ((= 1 wc)
+	   (3w-split-2-with-size-from-1 size))
 	  ((= 2 wc)
-	   (when (xor is-cs should-cs)
-	     (let* ((ob (window-buffer (next-window))))
-	       (delete-other-windows)
-	       (let* ((nw (if should-cs (split-window-right) (split-window-below))))
-		 (set-window-buffer nw ob))))
-	   (balance-windows))
-	  
+	   (3w-split-2-with-size-from-1 size (window-buffer (next-window))))
 	  ((= 3 wc)
-	   (let* ((sw (car (window-at-side-list nil (if cs 'right 'bottom))))
-		  (sb (window-buffer sw)))
-	     (when (string-match-p 3w-side-window-rx (buffer-name sb))
-	       (setq 3w-side-buffer-store))
-	     (delete-window sw)
-	     (balance-windows)))
-	  
+	   (3w-split-2-with-size-from-1 size (window-buffer (next-window))))
 	  (t
-	   (delete-other-windows)
-	   (3w-split-1-1)) ;; ie wc == 1
-	  )))
+	   (3w-split-2-with-size-from-1 size)))))
+
+(defun 3w-split-3-from-1 (&optional next-buffer next-next-buffer)
+  (message "-from-1: nb=%s nnb=%s" next-buffer next-next-buffer)
+  (3w-store-side-buffer)
+  (delete-other-windows)
+  (let* ((nb (or next-buffer (other-buffer)))
+	 (nnb (or next-next-buffer
+		  (unless (eq nb 3w-side-buffer-store) 3w-side-buffer-store)
+		  (other-buffer nb)))
+	 (ow (selected-window))
+	 (nw (3w-split-window 3))
+	 (nnw (progn (select-window nw) (3w-split-window 3))))
+    (set-window-buffer nw nb)
+    (set-window-buffer nnw nnb)
+    (select-window ow)
+    (balance-windows)))
+
+(defun 3w-split-3 ()
+  "Produces three evenly split windows"
+  (interactive)
+  (let* ((wc (count-windows))
+	 (nb (window-buffer (next-window)))
+	 (nnb (window-buffer (next-window (next-window)))))
+    (balance-windows)
+    (cond ((= 1 wc)
+	   (3w-split-3-from-1))
+	  ((= 2 wc)
+	   (3w-split-3-from-1 nb))
+	  ((= 3 wc)
+	   (3w-split-3-from-1 nb nnb))
+	  (t
+	   (3w-split-3-from-1)))))
 
 ;; (3w-split-1)
-;; (3w-split-1-1)
+;; (3w-split-2)
+;; (3w-split-2-1)
+;; (3w-split-3)
 
 ;; TODO is it possible to listen / add hook to frame resize
+;; TODO test cases
 
 (provide '3w)
