087d20a4e606f3e83a22bcbe025e1570d33fb387
Felix Geller <fgeller@gmail.com>
2021-01-03 11:17:06 +0100

    emacs: hlt: prefer active region

--- .emacs.d/appearance.el
+++ .emacs.d/appearance.el
@@ -27,6 +27,7 @@
 
 (install 'highlight-thing)
 (global-highlight-thing-mode +1)
+(setq highlight-thing-prefer-active-region t)
 
 (defun fg/add-todo-keyword ()
   (font-lock-add-keywords nil '(("\\(TODO\\|FIXME\\)" 1 font-lock-warning-face prepend))))
