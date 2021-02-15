40968babc7cc81abc077e7d1265c4bd1ae7ff042
Felix Geller <fgeller@gmail.com>
2021-02-16 00:52:47 +0100

    emacs: adds many lsp config options

--- .emacs.d/lsp.el
+++ .emacs.d/lsp.el
@@ -4,8 +4,20 @@
 
 (setq
  lsp-session-file "~/.emacs.d/transient/lsp-session-v1"
- lsp-eldoc-render-all t
+ lsp-eldoc-render-all nil
+ lsp-modeline-diagnostics-scope :file
+ lsp-file-watch-threshold 500
+ lsp-enable-file-watchers nil
  lsp-ui-doc-enable nil
+ lsp-ui-doc-delay 0.75
+ lsp-ui-doc-max-height 200
  lsp-ui-sideline-enable nil
+ lsp-enable-folding nil
+ lsp-enable-text-document-color nil
+ lsp-enable-on-type-formatting nil
+ ;; lsp-ui-sideline-show-hover nil
+ ;; lsp-ui-sideline-show-code-actions nil
+ ;; lsp-ui-sideline-show-diagnostics nil
+ lsp-headerline-breadcrumb-enable nil
  lsp-prefer-capf t
- lsp-idle-delay 0.1)
+ lsp-idle-delay 1)
