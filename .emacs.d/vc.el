5496600b2d894afd276f3eb5d7202002ee3e41ff
Felix Geller <fgeller@gmail.com>
2021-02-16 00:54:06 +0100

    emacs: drops git-gutter and git-messenger

--- .emacs.d/vc.el
+++ .emacs.d/vc.el
@@ -11,17 +11,3 @@
 
 (install 'magit)
 (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-
-(install 'git-gutter)
-(setq git-gutter:window-width 1)
-(setq git-gutter:modified-sign " ")
-(setq git-gutter:added-sign " ")
-(setq git-gutter:deleted-sign " ")
-(setq git-gutter:separator-sign " ")
-(setq git-gutter:always-show-separator t)
-(setq git-gutter:unchanged-sign " ")
-
-(global-git-gutter-mode +1)
-
-(install 'git-messenger)
-(setq git-messenger:use-magit-popup t)
