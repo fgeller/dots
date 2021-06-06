5ca0d8f1243ed0487f0289e202e3fdc2c469a54e
Felix Geller <fgeller@gmail.com>
2021-06-06 23:10:25 +0200

    emacs: minimizes lsp config

--- .emacs.d/lsp.el
+++ .emacs.d/lsp.el
@@ -1,23 +1,11 @@
 (install 'lsp-mode)
 (install 'lsp-ui)
-;; (install 'company-lsp)
 
 (setq
- lsp-session-file "~/.emacs.d/transient/lsp-session-v1"
- lsp-eldoc-render-all nil
- lsp-modeline-diagnostics-scope :file
- lsp-file-watch-threshold 500
- lsp-enable-file-watchers nil
- lsp-ui-doc-enable nil
- lsp-ui-doc-delay 0.75
- lsp-ui-doc-max-height 200
- lsp-ui-sideline-enable nil
- lsp-enable-folding nil
- lsp-enable-text-document-color nil
- lsp-enable-on-type-formatting nil
- ;; lsp-ui-sideline-show-hover nil
- ;; lsp-ui-sideline-show-code-actions nil
- ;; lsp-ui-sideline-show-diagnostics nil
+ lsp-ui-sideline-show-hover nil
+ lsp-ui-doc-show-with-cursor nil 
+ lsp-ui-doc-position 'at-point
+ lsp-modeline-diagnostics-scope :workspace
  lsp-headerline-breadcrumb-enable nil
  lsp-prefer-capf t
- lsp-idle-delay 1)
+ lsp-file-watch-threshold 500)
