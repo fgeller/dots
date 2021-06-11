39d260bf7a77f5069595fa336d5ff04829d07bf5
Felix Geller <fgeller@gmail.com>
2021-06-11 23:42:58 +0200

    emacs: evil tweaks.
    add bindings to open line above & below without insert
    
    adds keymap for emacs lisp and go mode

--- .emacs.d/evil.el
+++ .emacs.d/evil.el
@@ -1,5 +1,7 @@
+(install 'undo-fu)
 (setq evil-want-keybinding nil) ;; prefer evil-collection
 (install 'evil)
+(setq evil-undo-system 'undo-fu)
 (evil-mode 1)
 
 (require 'evil-workman-mode)
@@ -7,11 +9,6 @@
 
 (install 'evil-collection)
 (evil-collection-init)
-
-(dolist (map (list magit-mode-map
-		   compilation-mode-map
-		   help-mode-map))
-  (evil-define-key 'normal map (kbd "SPC") fg/leader-keymap))
 
 (install 'evil-surround)
 (global-evil-surround-mode 1)
@@ -38,14 +35,45 @@
   (define-key map (kbd "w") 'leerzeichen-mode)
   (define-key map (kbd "d") 'toggle-debug-on-error))
 
+(defun fg/open-line-below-normal ()
+  (interactive)
+  (save-excursion
+    (forward-line 1)
+    (end-of-line)
+    (insert "\n")))
+
+(defun fg/open-line-above-normal ()
+  (interactive)
+  (save-excursion
+    (forward-line -1)
+    (end-of-line)
+    (insert "\n")))
+
+(defconst fg/elisp-eval-keymap (make-keymap))
+(define-key fg/elisp-eval-keymap (kbd "r") 'eval-region)
+(define-key fg/elisp-eval-keymap (kbd "b") 'eval-buffer)
+
+(defconst fg/go-keymap (make-keymap))
+(let ((map fg/go-keymap))
+  (define-key map (kbd "t") 'go-run-all-tests)
+  (define-key map (kbd "T") 'go-run-this-test)
+  (define-key map (kbd "e") 'go-goto-first-error)
+  (define-key map (kbd "n") 'go-goto-next-error)
+  (define-key map (kbd "p") 'go-goto-previous-error)
+  (define-key map (kbd "g") 'go-play))
+
 (defconst fg/leader-keymap (make-keymap))
 (let ((map fg/leader-keymap))
   (define-key map (kbd "SPC") 'consult-buffer)
   (define-key map (kbd "TAB") 'indent-for-tab-command)
   (define-key map (kbd "cC") 'compile)
   (define-key map (kbd "cc") 'recompile)  
+  (define-key map (kbd "d") fg/go-keymap)
+  (define-key map (kbd "e") fg/elisp-eval-keymap)
   (define-key map (kbd "g") 'consult-ripgrep)
   (define-key map (kbd "G") 'deadgrep)
+  (define-key map (kbd "l") 'fg/open-line-below-normal)
+  (define-key map (kbd "L") 'fg/open-line-above-normal)
   (define-key map (kbd "m") 'magit-status)
   (define-key map (kbd "p") 'consult-yank-from-kill-ring)
   (define-key map (kbd "q") 'query-replace)
@@ -59,5 +87,16 @@
   (define-key map (kbd "xx") 'execute-extended-command))
 
 (evil-define-key '(visual normal) 'global (kbd "SPC") fg/leader-keymap)
+
+(after 'magit
+  (evil-define-key 'normal magit-mode-map (kbd "SPC") fg/leader-keymap))
+(after 'compile
+  (evil-define-key 'normal compilation-mode-map (kbd "SPC") fg/leader-keymap))
+(after 'custom
+  (evil-define-key 'normal custom-mode-map (kbd "SPC") fg/leader-keymap))
+(after 'help
+  (evil-define-key 'normal help-mode-map (kbd "SPC") fg/leader-keymap))
+(after 'view
+  (evil-define-key 'normal view-mode-map (kbd "SPC") fg/leader-keymap))
 
 (setq evil-lookup-func 'lsp-describe-thing-at-point)
