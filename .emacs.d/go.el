18598f48d68b2e826c702a1a5e33dfc54d578848
Felix Geller <fgeller@gmail.com>
2021-06-13 00:45:12 +0200

    emacs: more tweaks to golang helpers

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -1,6 +1,6 @@
 (install 'go-mode)
 
-(defun golang-customizations ()
+(defun fg/golang-customizations ()
   (defalias 'go-play-buffer nil)
   (defalias 'go-play-region nil)
 
@@ -18,81 +18,94 @@
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 
-(add-hook 'go-mode-hook 'golang-customizations)
+(add-hook 'go-mode-hook 'fg/golang-customizations)
 
-(defun go-tests-buffer-name ()
-  (format "*go-test[%s in %s]*"
+(defun fg/go-compilation-buffer-name ()
+  (format "*go-compilation[%s in %s]*"
           (file-name-nondirectory (directory-file-name default-directory))
           (file-name-directory (directory-file-name default-directory))))
 
-(defun go-tests-toggle-truncate-lines ()
+(defun fg/go-compilation-toggle-truncate-lines ()
   (interactive)
-  (let ((buf (get-buffer (go-tests-buffer-name))))
+  (let ((buf (get-buffer (fg/go-compilation-buffer-name))))
     (when buf (with-current-buffer buf (toggle-truncate-lines)))))
 
 
-(defun go-run-this-test ()
+(defun fg/go-run-this-test ()
   (interactive)
-  (go-run-tests (format "go test -v -run %s"
+  (fg/go-run (format "go test -v -run %s"
                         (save-excursion
                           (re-search-backward "func \\(Test.+\\)(" (point-min))
                           (match-string 1)))))
 
-(defun go-build-this ()
+(defun fg/go-build-this ()
   (interactive)
-  (go-run-tests (format "go build .")))
+  (fg/go-run (format "go build .")))
 
-(defun go-run-all-tests ()
+(defun fg/go-run-this ()
   (interactive)
-  (go-run-tests "go test -v"))
+  (fg/go-run "go run *.go"))
 
-(defun go-run-tests (cmd)
+(defun fg/go-run-all-tests ()
+  (interactive)
+  (fg/go-run "go test -v"))
+
+(defun fg/go-run (cmd)
   (let* ((dir (file-name-nondirectory (directory-file-name default-directory)))
-         (buf (go-tests-buffer-name)))
+         (buf (fg/go-compilation-buffer-name)))
     (if (get-buffer buf) (with-current-buffer buf (compile cmd))
       (compile cmd)
       (with-current-buffer "*compilation*" (rename-buffer buf)))))
 
-(defun go-make-build ()
+(defun fg/go-make-build ()
   (interactive)
   (let* ((dir (file-name-nondirectory (directory-file-name default-directory)))
-         (buf (go-tests-buffer-name)))
+         (buf (fg/go-compilation-buffer-name)))
     (if (get-buffer buf) (with-current-buffer buf (compile "make build"))
       (compile "make build")
       (with-current-buffer "*compilation*" (rename-buffer buf)))))
 
-(defmacro go-goto-error (&rest body)
-  `(let ((buf (go-tests-buffer-name)))
+(defun fg/go-make-tests ()
+  (interactive)
+  (let* ((dir (file-name-nondirectory (directory-file-name default-directory)))
+         (buf (fg/go-compilation-buffer-name)))
+    (if (get-buffer buf) (with-current-buffer buf (compile "make test"))
+      (compile "make test")
+      (with-current-buffer "*compilation*" (rename-buffer buf)))))
+
+
+(defmacro fg/go-goto-error (&rest body)
+  `(let ((buf (fg/go-compilation-buffer-name)))
      (when (get-buffer buf)
        (with-current-buffer buf
          ,@body
          (compile-goto-error)))))
 
-(defun go-goto-first-error ()
+(defun fg/go-goto-first-error ()
   (interactive)
-  (go-goto-error (goto-char (point-min)) (compilation-next-error 1)))
+  (fg/go-goto-error (goto-char (point-min)) (compilation-next-error 1)))
 
-(defun go-goto-next-error ()
+(defun fg/go-goto-next-error ()
   (interactive)
-  (go-goto-error (compilation-next-error 1)))
+  (fg/go-goto-error (compilation-next-error 1)))
 
-(defun go-goto-previous-error ()
+(defun fg/go-goto-previous-error ()
   (interactive)
-  (go-goto-error (compilation-previous-error 1)))
+  (fg/go-goto-error (compilation-previous-error 1)))
 
-(defun go-ignore-all-tests ()
+(defun fg/go-ignore-all-tests ()
   (interactive)
   (save-excursion
     (goto-char (point-min))
     (replace-regexp "func Test\\([^(]+\\)(" "func IgnoreTest\\1(" nil (point-min) (point-max))))
 
-(defun go-enable-all-tests ()
+(defun fg/go-enable-all-tests ()
   (interactive)
   (save-excursion
     (goto-char (point-min))
     (replace-regexp "func IgnoreTest\\([^(]+\\)(" "func Test\\1(" nil (point-min) (point-max))))
 
-(defun go-play ()
+(defun fg/go-play ()
   (interactive)
   (let* ((temporary-file-directory (expand-file-name "tmp/" (getenv "GOPATH")))
          (temp-file (progn
