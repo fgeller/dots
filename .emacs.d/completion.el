431741e1ecb35202fc913b13f503fb7b403587c4
Felix Geller <fgeller@gmail.com>
2021-07-18 21:32:03 +0200

    emacs: adds consult-flycheck and -lsp.

--- .emacs.d/completion.el
+++ .emacs.d/completion.el
@@ -4,6 +4,9 @@
 (global-company-mode)
 
 (install 'consult)
+(install 'consult-flycheck)
+(install 'consult-lsp)
+
 (recentf-mode)
 (setq consult-project-root-function (lambda () (locate-dominating-file "." ".git")))
   (setq xref-show-xrefs-function #'consult-xref
@@ -23,4 +26,35 @@
 (define-key minibuffer-local-map (kbd "C-.") 'fg/zap-back-till-/)
 
 (setq enable-recursive-minibuffers t) 
+
+(defconst fg/consult--source-git-ls-files
+  `(:name     "Git ls-files"
+    :narrow   (?g . "Git ls-files")
+    :category file
+    :face     consult-file
+    :history  file-name-history
+    :state    ,#'consult--file-state
+    :enabled  ,(lambda () (and consult-project-root-function))
+    :items
+    ,(lambda ()
+      (when-let (root (consult--project-root))
+	(let* ((default-directory root)
+	       (cmd (format "git ls-files --full-name"))
+	       (files (split-string (shell-command-to-string cmd) "\n" t))
+	       (abs-files (mapcar (lambda (fn) (expand-file-name fn root)) files))
+	       )
+	  abs-files))))
+  "Git ls-files candidate source for `consult-buffer'.")
+
+(after 'consult
+  (add-to-list 'consult-buffer-sources fg/consult--source-git-ls-files)
+
+ (consult-customize
+  consult-ripgrep consult-git-grep consult-grep
+  consult-bookmark consult-recent-file consult-xref
+  consult--source-file consult--source-project-file consult--source-bookmark
+  fg/consult--source-git-ls-files
+  :preview-key (kbd "M-."))
+
+  )
 
