9a22284f2bc28368454645305656a23f2f91613b
Felix Geller <fgeller@gmail.com>
2021-07-18 21:03:41 +0200

    emacs: re-introduces modeline toggle for modal-mode

--- .emacs.d/mode-line.el
+++ .emacs.d/mode-line.el
@@ -15,3 +15,30 @@
 		;;mode-line-modes
 		mode-line-misc-info
 		mode-line-end-spaces))
+
+(defun modal-mode-visual-toggle ()
+  (interactive)
+  (let ((faces-to-toggle '(mode-line)))
+    (cond (modal-mode
+	   (mapcar (lambda (face)
+		     (set-face-background face "#1565c0")
+		     (set-face-foreground face "white"))
+		   faces-to-toggle))
+	  (t
+	   (mapcar (lambda (face)
+		     (set-face-background face "#ffd54f")
+		     (set-face-foreground face "black"))
+		   faces-to-toggle)))))
+(add-hook 'modal-mode-hook 'modal-mode-visual-toggle)
+
+(defun mode-line-bell ()
+  (let ((orig-active (face-attribute 'mode-line :background)))
+    (set-face-attribute 'mode-line nil :background "#f44336")
+    (sit-for 0.1 t)
+    (set-face-attribute 'mode-line nil :background orig-active)))
+
+(setq
+ visible-bell nil
+ ring-bell-function 'mode-line-bell)
+
+	   
