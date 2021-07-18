46c063d197a8448eebac7013722b93e29f6b3db2
Felix Geller <fgeller@gmail.com>
2021-07-18 21:06:32 +0200

    emacs: basic config for yasnippet

--- .emacs.d/snippets.el
+++ .emacs.d/snippets.el
@@ -1,31 +1,6 @@
-(defconst snippet-point-marker "@@@"
-  "Marker to identify location of point in snippet after expansion")
+(install 'yasnippet)
 
-(defconst snippets
-  '(
-    ("ife" . "if err != nil {
-	return @@@err
-}")
-    )
-  "Alist of snippet keys to expanded version")
+(setq yas-snippet-dir '("~/.emacs.d/snippets/"))
 
-
-(defun try-snippet-expansion ()
-  "Tries to identify snippet key at point using and replaces key by snippet if applicable"
-  (interactive)
-  (let* ((tt 'symbol)
-	 (tap (thing-at-point tt))
-	 (sa (assoc tap snippets)))
-    (if (not sa) (message "found not snippet for [%s]" tap)
-      (let* ((bds (bounds-of-thing-at-point tt)))
-	(kill-region (car bds) (cdr bds))
-	(insert (cdr sa))
-	(let* ((sp (point))
-	       (lm (- sp (length (cdr sa)))))
-	  (indent-region lm sp)
-	  ;; go to marker and cleanup if applicable
-	  (search-backward snippet-point-marker lm t)
-	  (unless (eq sp (point))
-	    (delete-forward-char (length snippet-point-marker))))))))
-
-(define-key global-map (kbd "C-'") 'try-snippet-expansion)
+(after 'yasnippet
+  (yas-reload-all))
