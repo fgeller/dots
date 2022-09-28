e8b9d2a1c49871be2bf5d49928c92ca82261fdbb
Felix Geller <fgeller@protonmail.com>
2022-09-28 09:55:42 +0200

    emacs: dap mode tweaks

--- .emacs.d/dap.el
+++ .emacs.d/dap.el
@@ -1,5 +1,13 @@
-;; (install 'dap-mode)
-(install 'dap-dlv-go)
+(install 'dap-mode)
+(install 'dap-dlv-go 'require)
 
-(after 'go-mode
-  (require 'dap-go))
+(setq dap-dlv-go-delve-path (expand-file-name "~/bin/dlv"))
+(dap-mode 1)
+(dap-ui-mode 1)
+(dap-tooltip-mode 1)
+(tooltip-mode 1)
+(dap-ui-controls-mode 1)
+
+;; (setq dap-print-io t)
+;; https://emacs-lsp.github.io/dap-mode/page/configuration/#go
+;; https://github.com/emacs-lsp/dap-mode/commit/6b2cca8fd5aa3ae21a3b9143ec987c487f9e913d#commitcomment-84428823
