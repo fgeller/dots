e3b38ab757627a5e3a5afe6db4ef0ea4ff00c4d2
Felix Geller <fgeller@protonmail.com>
2024-10-04 16:50:59 +0200

    emacs: yaml config

--- .emacs.d/yaml.el
+++ .emacs.d/yaml.el
@@ -1,2 +1,13 @@
-(use-package yaml-mode
-  :mode ("\\.yaml\\'" . yaml-mode))
+(use-package yaml-ts-mode
+  :mode ("\\.yaml\\'" . yaml-ts-mode)
+  :config 
+
+  (defun fg/yaml-mode-hook ()
+	(eldoc-mode 1)
+	(yas-minor-mode)
+	(setq indent-tabs-mode nil)
+	(lsp-mode))
+  
+  (add-hook 'yaml-ts-mode-hook 'fg/yaml-mode-hook)
+
+)
