5fc5dab9ae597a563e1f8e43b8885473190f449b
Felix Geller <fgeller@protonmail.com>
2024-10-04 16:48:46 +0200

    emacs: dap config

--- .emacs.d/dap.el
+++ .emacs.d/dap.el
@@ -1,13 +1,7 @@
-(install 'dap-mode)
-(install 'dap-dlv-go 'require)
-
-(setq dap-dlv-go-delve-path (expand-file-name "~/bin/dlv"))
-(dap-mode 1)
-(dap-ui-mode 1)
-(dap-tooltip-mode 1)
-(tooltip-mode 1)
-(dap-ui-controls-mode 1)
-
-;; (setq dap-print-io t)
-;; https://emacs-lsp.github.io/dap-mode/page/configuration/#go
-;; https://github.com/emacs-lsp/dap-mode/commit/6b2cca8fd5aa3ae21a3b9143ec987c487f9e913d#commitcomment-84428823
+(use-package dap-mode 
+  :ensure t 
+  :commands (dap-mode)
+  :config
+  (require 'dap-dlv-go)
+  (setq dap-auto-configure-features '(sessions locals controls))
+  (setq dap-dlv-go-delve-path (expand-file-name "~/bin/dlv")))
