1a6373cdc0c985e6eb67f229b273628dea06a880
Felix Geller <fgeller@protonmail.com>
2024-12-23 09:47:06 +0100

    emacs: ensure search packages

--- .emacs.d/search.el
+++ .emacs.d/search.el
@@ -1,10 +1,12 @@
-(use-package wgrep :commands (wgrep-setup wgrep-rg))
+(use-package wgrep 
+  :ensure t 
+  :commands (wgrep-setup wgrep-rg))
 (use-package rg 
+  :ensure t
   :commands (rg)
   :config
   (require wgrep-rg)
-  (rg-enable-default-bindings)
-)
+  (rg-enable-default-bindings))
 
 (setq isearch-lazy-count t)
 
